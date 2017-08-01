3d98d354dbaaf89fe207b1a11889b0458a487c1d
Bernardo Vieira <bernardo5@live.com.pt>
2017-08-01 17:14:36 +0100

    rest api request

--- ceeloko/src/index.php
+++ ceeloko/src/index.php
@@ -15,3 +15,13 @@ use HelloWorld\SayHello as Say;
 $say = new Say(2);
 
 echo $say->world();
+
+$service_url = 'http://localhost:3000/users/';
+$curl = curl_init($service_url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST, false);
+$curl_response = curl_exec($curl);
+curl_close($curl);
+
+$decoded = json_decode($curl_response);
+echo $decoded;
